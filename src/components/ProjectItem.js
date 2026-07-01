78453f338b21169fe8dafb8fc43d559697e62a05
Marc Bryan Mata <marc.bryan609@gmail.com>
2026-07-02 01:53:51 +0800

    update link

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function ProjectItem({ image, name, id }) {
+function ProjectItem({ image, name, id, link }) {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (link && link.startsWith("/")) {
+      navigate(link);
+      return;
+    }
+
+    if (link && link.startsWith("http")) {
+      window.open(link, "_blank", "noopener,noreferrer");
+      return;
+    }
+
+    navigate("/project/" + id);
+  };
+
   return (
-    <div
-      className="projectItem"
-      onClick={() => {
-        navigate("/project/" + id);
-      }}
-    >
+    <div className="projectItem" onClick={handleClick}>
       <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
       <h1> {name} </h1>
     </div>
